6acd9bd6e6c194ccaf1d6150c2047ddd88cfd38b
Himanshu <Himanshu@gmail.com>
2023-10-19 12:22:21 +0530

    himanshu

--- src/main/java/com/example/onlineshoping/OnlineShopingApplication.java
+++ src/main/java/com/example/onlineshoping/OnlineShopingApplication.java
@@ -7,9 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class OnlineShopingApplication {
 
 	public static void main(String[] args) {
-		
+
 		SpringApplication.run(OnlineShopingApplication.class, args);
-		System.out.println("Application Started");
+		System.out.println("Application Started.....");
 		
 	}
 
