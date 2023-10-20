097724878fa6eba2f14b750dba5d251227773df6
Himanshu <Himanshu@gmail.com>
2023-10-20 13:31:29 +0530

    wrapper classes

---
+++ src/main/java/com/example/onlineshoping/wrapperclasses/UserWrapper.java
@@ -0,0 +1,10 @@
+package com.example.onlineshoping.wrapperclasses;
+
+import com.example.onlineshoping.entity.User;
+import lombok.Data;
+
+@Data
+public class UserWrapper{
+	private User user;
+
+}
