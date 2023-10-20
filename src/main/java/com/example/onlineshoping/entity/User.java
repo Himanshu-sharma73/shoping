4604f707e53ddef187776d20d93acb17b57145a6
Sandeepkumar2360 <sandeep200192@gmail.com>
2023-10-20 15:00:33 +0530

    set

--- src/main/java/com/example/onlineshoping/entity/User.java
+++ src/main/java/com/example/onlineshoping/entity/User.java
@@ -4,15 +4,19 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity(name = "userdetails")
 public class User {
-
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private int id;
-    
     
     private String name;
     private long mobileNo;
