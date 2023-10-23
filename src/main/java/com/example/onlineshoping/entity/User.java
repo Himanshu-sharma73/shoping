38fca3e1553f9c1487e7d2cd06418a6572c726d5
kranujsaini <kranujsaini@gmail.com>
2023-10-23 13:34:27 +0530

    add validation

--- src/main/java/com/example/onlineshoping/entity/User.java
+++ src/main/java/com/example/onlineshoping/entity/User.java
@@ -4,7 +4,14 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.validation.constraints.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+//import jakarta.validation.constraints.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -15,23 +22,24 @@ import lombok.NoArgsConstructor;
 @Entity(name = "userdetails")
 public class User {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private int id;
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private int id;
-    
-    @Size(min = 3,message = "User name should have at least three Character")
-    private String name;
-    
-    
-    @Min(10) @Max(10)
-    private long mobileNo;
+	@Size(min = 3, message = "User name should have at least three Character")
+	private String name;
 
-    @Email(message = "Enter Correct Email")
-    private String email;
+	@Min(value = 1000000000L, message = "Mobile number must be at least 10 digits.")
+	@Max(value = 9999999999L, message = "Mobile number must not exceed 10 digits.")
+	private long mobileNo;
 
-    @NotBlank(message = "Address should not be Blank")
-    @NotNull(message = "Address should not be Null")
-    private String address;
+	@NotBlank(message = "Email address cannot be blank")
+	@Email(message = "Invalid email address")
+	private String email;
+
+	@NotBlank(message = "Address should not be Blank")
+	@NotNull(message = "Address should not be Null")
+	
+	private String address;
 
 }
