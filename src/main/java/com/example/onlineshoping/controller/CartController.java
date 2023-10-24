86be394aed2929af2479c53350fa6c0adb5cfc3d
Himanshu <Himanshu@gmail.com>
2023-10-24 11:16:59 +0530

    product validation

--- src/main/java/com/example/onlineshoping/controller/CartController.java
+++ src/main/java/com/example/onlineshoping/controller/CartController.java
@@ -9,12 +9,14 @@ import com.example.onlineshoping.entity.Cart;
 import com.example.onlineshoping.entity.Item;
 import com.example.onlineshoping.entity.Product;
 import com.example.onlineshoping.entity.User;
+import com.example.onlineshoping.exception.ApiResponse;
+import com.example.onlineshoping.exception.ResourceNotFoundException;
+import com.example.onlineshoping.wrapperclasses.ProductListWrapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
@@ -40,12 +42,12 @@ public class CartController {
     public ResponseEntity<String> addToCart(@PathVariable int userId, @PathVariable int productId) {
         Optional<User> optionalUser=userRepository.findById(userId);
         if (optionalUser.isPresent()) {
-            Optional<Cart> optional = cartRepository.findByUserId(userId);
-            if (optional.isPresent()) {
-                Cart cart = optional.get();
+            Optional<Cart> optionalCart = cartRepository.findByUserId(userId);
+            if (optionalCart.isPresent()) {
+                Cart cart = optionalCart.get();
                 Optional<Product> optionalProduct = productRepository.findById(productId);
                 if(optionalProduct.isEmpty()){
-                    return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Product is not Present");
+                    throw  new ResourceNotFoundException("Product","id:", productId,"1002");
                 }
                 Product product=optionalProduct.get();
                 Item item = new Item(product, cart);
@@ -62,23 +64,59 @@ public class CartController {
                 return ResponseEntity.ok("Product added to cart successfully");
             }
         }
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body("User Not present First Create the User");
+       throw new ResourceNotFoundException("User","id:",userId,"1001");
 
     }
 
     @GetMapping("/users/{userId}/cart")
-    public List<Product> getCart(@PathVariable int userId) {
+    public ResponseEntity<ApiResponse> getCart(@PathVariable int userId) {
         Optional<User> optionalUser = userRepository.findById(userId);
         if (optionalUser.isPresent()) {
             User user = optionalUser.get();
             Optional<Cart> optionalCart = cartRepository.findByUserId(user.getId());
+            Cart cart;
             if (optionalCart.isPresent()) {
-                Cart cart = optionalCart.get();
+                cart = optionalCart.get();
                 List<Item> cartItems = itemRepository.findByCartId(cart.getId());
+                List<Product> products = cartItems.stream().map(i -> i.getProduct()).collect(Collectors.toList());
 
+
+                double totalMRP=0;
+                int totalDiscountPercentage=0;
+                long totalTax=0;
+                double totalCost=0;
+                for (int i=0;i<products.size();i++){
+                    Product product=products.get(i);
+                    totalMRP+=product.getMrp();
+                    totalDiscountPercentage+=product.getDiscountPercentage();
+                    totalTax+=product.getTax();
+                }
+
+                totalCost=((totalMRP+(totalMRP*totalTax/100))*(100-totalDiscountPercentage))/100;
+
+                ProductListWrapper productListWrapper=new ProductListWrapper();
+                productListWrapper.setTotalMRP(totalMRP);
+                productListWrapper.setTotalDiscountPercentage(totalDiscountPercentage);
+                productListWrapper.setTotalTax(totalTax);
+                productListWrapper.setTotalCost(totalCost);
+
+                ApiResponse apiResponse=new ApiResponse();
+                productListWrapper.setProductList(products);
+                apiResponse.setData(productListWrapper);
+                return  new ResponseEntity<ApiResponse>(apiResponse,HttpStatus.OK);
+            }
+            else {
+                ApiResponse apiResponse=new ApiResponse();
+                ProductListWrapper productListWrapper=new ProductListWrapper();
+                productListWrapper.setProductList(null);
+                apiResponse.setData(productListWrapper);
+                return  new ResponseEntity<ApiResponse>(apiResponse,HttpStatus.OK);
             }
         }
-        return null;
+        else {
+            throw new ResourceNotFoundException("User","Id:",userId,"1001");
+        }
+
     }
 
 
