7aa683e9d3cd9aa4223f9c179f724de9d87e1812
kranujsaini <kranujsaini@gmail.com>
2023-10-24 11:03:03 +0530

    change apiresponse

--- src/main/java/com/example/onlineshoping/controller/CartController.java
+++ src/main/java/com/example/onlineshoping/controller/CartController.java
@@ -9,6 +9,11 @@ import com.example.onlineshoping.entity.Cart;
 import com.example.onlineshoping.entity.Item;
 import com.example.onlineshoping.entity.Product;
 import com.example.onlineshoping.entity.User;
+import com.example.onlineshoping.exception.ApiResponse;
+import com.example.onlineshoping.exception.ResourceNotFoundException;
+import com.example.onlineshoping.wrapperclasses.ProductWrapper;
+import com.example.onlineshoping.wrapperclasses.UserWrapper;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -37,7 +42,7 @@ public class CartController {
 
 
     @PostMapping("/addtocart/{userId}/product/{productId}")
-    public ResponseEntity<String> addToCart(@PathVariable int userId, @PathVariable int productId) {
+    public ResponseEntity<ApiResponse> addToCart(@PathVariable int userId, @PathVariable int productId) {
         Optional<User> optionalUser=userRepository.findById(userId);
         if (optionalUser.isPresent()) {
             Optional<Cart> optional = cartRepository.findByUserId(userId);
@@ -45,25 +50,39 @@ public class CartController {
                 Cart cart = optional.get();
                 Optional<Product> optionalProduct = productRepository.findById(productId);
                 if(optionalProduct.isEmpty()){
-                    return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Product is not Present");
+                  throw new ResourceNotFoundException("Product", "Id:", productId, "1002");
                 }
                 Product product=optionalProduct.get();
                 Item item = new Item( product, cart);
                 itemRepository.save(item);
-                return ResponseEntity.ok("Product added to cart successfully");
+                ApiResponse apiResponse = new ApiResponse();
+                ProductWrapper productWrapper = new ProductWrapper();
+                productWrapper.setProduct(product);
+                apiResponse.setData(productWrapper);
+                return new ResponseEntity<ApiResponse> (apiResponse,HttpStatus.OK);
             } else {
                 Cart cart1 = new Cart();
                 User user = userRepository.findById(userId).get();
                 cart1.setUser(user);
                 cartRepository.save(cart1);
-                Product product = productRepository.findById(productId).get();
+                Optional<Product>  optionalProduct = productRepository.findById(productId);
+                if(optionalProduct.isEmpty()){
+                    throw new ResourceNotFoundException("Product", "Id:", productId, "1002");
+                  }
+                Product product=optionalProduct.get();
                 Item item = new Item(product, cart1);
                 itemRepository.save(item);
-                return ResponseEntity.ok("Product added to cart successfully");
+                ApiResponse apiResponse = new ApiResponse();
+                ProductWrapper productWrapper = new ProductWrapper();
+                productWrapper.setProduct(product);
+                apiResponse.setData(productWrapper);
+                return new ResponseEntity<ApiResponse> (apiResponse,HttpStatus.OK);
             }
         }
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body("User Not present First Create the User");
-
+        else {
+        	throw new ResourceNotFoundException("User", "Id", userId, "1001");
+        }
+        
     }
 
     @GetMapping("/users/{userId}/cart")
