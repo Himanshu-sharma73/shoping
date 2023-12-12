64d7fb97590139bf66a48f8aa35ad008a3d54e24
Himanshu <Himanshu@gmail.com>
2023-12-12 22:57:58 +0530

    code updated

--- src/main/java/com/example/onlineshoping/wrapperclasses/ProductListWrapper.java
+++ src/main/java/com/example/onlineshoping/wrapperclasses/ProductListWrapper.java
@@ -1,14 +1,16 @@
 package com.example.onlineshoping.wrapperclasses;
 
 import com.example.onlineshoping.entity.Product;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.List;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class ProductListWrapper {
     private List<Product> productList;
-    private List<ProductDetails> productLists;
-    private double allTotalCost;
-
 }
